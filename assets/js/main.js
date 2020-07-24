f14fa288967c04b3de35fac8ce5d7ad6f4fd322a
Alessandro Pellegrini <a.pellegrini@ing.uniroma2.it>
2020-07-24 11:24:26 +0200

    Add swipe gesture to show menu
    A simple swipe gesture is added to show/hide the menu on mobiles and
    tablets.
    
    Signed-off-by: Alessandro Pellegrini <a.pellegrini@uniroma2.it>

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,6 @@ function fadeOut(elem, ms) {
     }
 }
 
-// Native fadeIn
 function fadeIn(elem, ms) {
     elem.style.opacity = 0;
 
@@ -48,4 +47,26 @@ function slide() {
         fadeIn(button, 200);
         shown = true;
     }
+}
+
+let xDown = null, yDown = null, xUp = null, yUp = null;
+document.addEventListener('touchstart', touchstart, false);
+document.addEventListener('touchmove', touchmove, false);
+document.addEventListener('touchend', touchend, false);
+function touchstart(evt) { const firstTouch = (evt.touches || evt.originalEvent.touches)[0]; xDown = firstTouch.clientX; yDown = firstTouch.clientY; }
+function touchmove(evt) { if (!xDown || !yDown ) return; xUp = evt.touches[0].clientX; yUp = evt.touches[0].clientY; }
+function touchend(evt) {
+    const xDiff = xUp - xDown, yDiff = yUp - yDown;
+    if ((Math.abs(xDiff) > Math.abs(yDiff)) && (Math.abs(xDiff) > 0.33 * document.body.clientWidth)) {
+        if (xDiff < 0) {
+            if (!shown) {
+                slide();
+            }
+        } else {
+            if (shown) {
+                slide();
+            }
+        }
+    }
+    xDown = null, yDown = null;
 }
